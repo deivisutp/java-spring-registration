6527b54c17aa8f0239ea508c4e88dc5ecd57f5b5
Deivis <deivisutp@gmail.com>
2021-04-20 00:33:15 -0300

    first

---
+++ src/main/java/com/example/demo/registration/RegistrationRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class RegistrationRequest {
+    private  final  String firstName;
+    private  final  String lastName;
+    private  final  String email;
+    private  final  String password;
+}
